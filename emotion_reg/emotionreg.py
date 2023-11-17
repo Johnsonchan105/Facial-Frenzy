7b218122cc9158208fd830c27a9a8ea718e93579
Johnson Chan <c_chan@ucsb.edu>
2023-11-16 16:09:04 -0800

    added new code for emotion reg

--- emotion_reg/emotionreg.py
+++ emotion_reg/emotionreg.py
@@ -1,45 +1,10 @@
-import tensorflow as tf
-import numpy as np
+from keras import Sequential
+from keras.models import load_model
 import cv2
-import keras
-import pandas as pd
-import h5py
-import mtcnn as MTCNN
+import numpy as np
+from keras.preprocessing.image import img_to_array
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+class_labels = {0: 'Angry', 1: 'Fear', 2: 'Happy', 3: 'Neutral', 4: 'Sad', 5: 'Surprise'}
+classes = list(class_labels.values())
 
-cv2.namedWindow('window_frame')
-video_capture = cv2.VideoCapture(0)
-
-while True:
-    # Read a frame from the video capture
-    ret, frame = video_capture.read()
-
-    if not ret:
-        break
-
-    # Convert the frame to grayscale (face detection works better in grayscale)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Detect faces in the grayscale frame
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
-
-    # Draw rectangles around the detected faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    # Display the frame with detected faces
-    cv2.imshow('Face Detection', frame)
-
-    # Break the loop when the 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release the video capture and close the OpenCV window
-video_capture.release()
-cv2.destroyAllWindows()
-
-
-
-
-
+print(class_labels)
